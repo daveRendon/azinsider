23cd053a6a30e991544febef045c0b25ea81c5b0
Dave Rendon <1934250+daveRendon@users.noreply.github.com>
2024-05-01 11:21:09 -0500

    add theme

---
+++ docs/theme/src/js/config.js
@@ -0,0 +1,5 @@
+export const COLOR_THEME_DARK = "dark"
+export const COLOR_THEME_LIGHT = "light"
+export const COLOR_THEME_AUTO = "auto"
+export const THEME = "hugo-geekdoc"
+export const TOGGLE_COLOR_THEMES = [COLOR_THEME_AUTO, COLOR_THEME_DARK, COLOR_THEME_LIGHT]
---
+++ docs/theme/src/js/index.js
@@ -0,0 +1,52 @@
+import Clipboard from "clipboard"
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  let clipboard = new Clipboard(".clip")
+
+  clipboard.on("success", function (e) {
+    const trigger = e.trigger
+
+    if (trigger.hasAttribute("data-copy-feedback")) {
+      trigger.classList.add("gdoc-post__codecopy--success", "gdoc-post__codecopy--out")
+      trigger.querySelector(".gdoc-icon.copy").classList.add("hidden")
+      trigger.querySelector(".gdoc-icon.check").classList.remove("hidden")
+
+      setTimeout(function () {
+        trigger.classList.remove("gdoc-post__codecopy--success", "gdoc-post__codecopy--out")
+        trigger.querySelector(".gdoc-icon.copy").classList.remove("hidden")
+        trigger.querySelector(".gdoc-icon.check").classList.add("hidden")
+      }, 3000)
+    }
+
+    e.clearSelection()
+  })
+
+  document.querySelectorAll(".highlight").forEach((highlightDiv) => createCopyButton(highlightDiv))
+})
+
+function createCopyButton(highlightDiv) {
+  const button = document.createElement("span")
+
+  let codeSelector = "pre > code"
+  if (highlightDiv.querySelector(".lntable")) {
+    codeSelector = ".lntable .lntd:last-child pre > code"
+  }
+
+  const codeContainer = highlightDiv.querySelector(codeSelector)
+  if (codeContainer !== null) {
+    const codeContent = codeContainer.innerText.trim()
+
+    button.classList.add("flex", "align-center", "justify-center", "clip", "gdoc-post__codecopy")
+    button.type = "button"
+    button.innerHTML =
+      '<svg class="gdoc-icon copy"><use xlink:href="#gdoc_copy"></use></svg>' +
+      '<svg class="gdoc-icon check hidden"><use xlink:href="#gdoc_check"></use></svg>'
+    button.setAttribute("data-clipboard-text", codeContent)
+    button.setAttribute("data-copy-feedback", "Copied!")
+    button.setAttribute("role", "button")
+    button.setAttribute("aria-label", "Copy")
+
+    highlightDiv.classList.add("gdoc-post__codecontainer")
+    highlightDiv.insertBefore(button, highlightDiv.firstChild)
+  }
+}
